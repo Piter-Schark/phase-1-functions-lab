a0608c0c74d9fac77842eb4dfc476fcfc9282608
Peter <pschoenbauer@gmail.com>
2023-03-18 21:02:58 -0400

    functions lab with distances and fare calculation

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const feet = 264
+const hq = 42
+const pricePerFoot = 0.02
+
+function distanceFromHqInBlocks (someBlocks){
+    let blocks
+    if (someBlocks <=hq){
+    blocks = hq - someBlocks
+}
+else if (someBlocks >hq) {
+blocks = someBlocks - hq
+}    
+return blocks
+}
+
+function distanceFromHqInFeet(someFeet){
+    distanceFromHqInBlocks(someFeet);
+    return distanceFromHqInBlocks(someFeet) * feet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * feet
+}
+
+function calculatesFarePrice(start, destination) {
+    distanceTravelledInFeet (start, destination) 
+    if (distanceTravelledInFeet (start, destination) <= 400){
+    return 0
+    }
+    else if ((distanceTravelledInFeet (start, destination) > 400) && (distanceTravelledInFeet (start, destination) <=2000)){
+    return (distanceTravelledInFeet (start, destination) - 400 ) * pricePerFoot
+    }
+    else if ((distanceTravelledInFeet (start, destination) > 2000) && (distanceTravelledInFeet (start, destination) <=2500)){
+        return 25
+    }
+    else (distanceTravelledInFeet (start, destination) >2500)
+    return 'cannot travel that far'
+}
